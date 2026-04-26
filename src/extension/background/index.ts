6914c422d6cbb5090360988ad93883bf76287e1d
Timi Olaosebikan <timilaosebikan@gmail.com>
2026-04-26 12:27:57 -0700

    [Feature]: Notification system for targetRetention (#57)
    * [Feature]: Notification system for targetRetention
    
    * [Feature]: Notification system for targetRetention
    
    * remove duplicate
    
    * remove other dup

--- src/extension/background/index.ts
+++ src/extension/background/index.ts
@@ -1,27 +1,38 @@
 /** Service-worker bootstrap for background lifecycle, alarms, and runtime routing. */
-import {mutateAppData} from "../../data/repositories/appDataRepository";
-import {ensureCourseData} from "../../domain/courses/courseProgress";
-import {assertAuthorizedRuntimeMessage, validateRuntimeMessage,} from "../runtime/validator";
+import { mutateAppData, STORAGE_KEY } from "../../data/repositories/appDataRepository";
+import { ensureCourseData } from "../../domain/courses/courseProgress";
+import {
+  assertAuthorizedRuntimeMessage,
+  validateRuntimeMessage,
+} from "../runtime/validator";
 
-import {maybeNotifyDueQueue} from "./notifications";
-import {fail} from "./responses";
-import {handleMessage} from "./router";
+import { maybeNotifyDueQueue, scheduleNextDueAlarm } from "./notifications";
+import { fail } from "./responses";
+import { handleMessage } from "./router";
 
 chrome.runtime.onInstalled.addListener(async () => {
   await mutateAppData((data) => {
     ensureCourseData(data);
     return data;
   });
-  chrome.alarms.create("due-check", {periodInMinutes: 60});
+  void maybeNotifyDueQueue();
+  void scheduleNextDueAlarm();
 });
 
 chrome.runtime.onStartup.addListener(() => {
-  chrome.alarms.create("due-check", {periodInMinutes: 60});
+  void maybeNotifyDueQueue();
+  void scheduleNextDueAlarm();
+});
+
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (area === "local" && STORAGE_KEY in changes) {
+    void scheduleNextDueAlarm();
+  }
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "due-check") {
-    void maybeNotifyDueQueue();
+    void maybeNotifyDueQueue().then(() => scheduleNextDueAlarm());
   }
 });
 
